289a78a8d0f57f21f916ffb1caf09842100535a0
Stefan <s18625@gmail.com>
2020-12-15 01:50:16 +0100

    prawie gotowe szablony

--- tin-projekt-Branicki-s18625/controllers/drinkController.js
+++ tin-projekt-Branicki-s18625/controllers/drinkController.js
@@ -1,15 +1,111 @@
+const DrinkRepository = require('../repository/sequelize/DrinkRepository');
+const IngreadientRepository = require('../repository/sequelize//IngreadientRepository');
+
+
+
 exports.showDrinkList = (req, res, next) => {
-    res.render('pages/drinks', {
-        navLocation: 'drinks'
-    });
+    DrinkRepository.getDrinks()
+        .then(drinks => {
+            res.render('pages/drinks', {
+                drinks: drinks,
+                navLocation: 'drinks'
+            });
+        });
 }
 exports.showAddDrinkForm = (req, res, next) => {
     res.render('pages/drink_form', {
+        drink: {},
+        pageTitle: 'Nowy Drink',
+        formMode: 'createNew',
+        btnLabel: 'Dodaj drinka',
+        formAction: '/drinks/add',
         navLocation: 'drinks'
     });
+    // res.render('pages/drink_form', {
+    //     navLocation: 'drinks'
+    // });
 }
+
+exports.showEditDrinkForm = (req, res, next) => {
+    const drinkId = req.params.drinkId;
+    DrinkRepository.getDrinkById(drinkId)
+        .then(drink => {
+            res.render('pages/drink_form', {
+                drink: drink,
+                formMode: 'edit',
+                pageTitle: 'Edycja drinka',
+                btnLabel: 'Edytuj drinka',
+                formAction: '/drinks/edit',
+                navLocation: 'drinks'
+            });
+        });
+};
+
 exports.showDrinkDetails = (req, res, next) => {
-    res.render('pages/drink_details', {
-        navLocation: 'drinks'
-    });
+    const drinkId = req.params.drinkId;
+    DrinkRepository.getDrinkById(drinkId)
+        .then(drink => {
+            res.render('pages/drink_form', {
+                drink: drink,
+                formMode: 'showDetails',
+                pageTitle: 'Szczegóły drinka',
+                formAction: '',
+                navLocation: 'drinks'
+            });
+        });
+}
+// exports.showDrinkDetails = (req, res, next) => {
+//     res.render('pages/drink_details', {
+//         navLocation: 'drinks'
+//     });
+// }
+exports.addDrink = (req, res, next) => {
+    const drinkData = {
+        ...req.body
+    };
+    DrinkRepository.createDrink(drinkData)
+        .then(result => {
+            res.redirect('/drinks');
+        });
+};
+
+exports.updateDrink = (req, res, next) => {
+    const drinkId = req.body._id;
+    const drinkData = {
+        ...req.body
+    };
+    DrinkRepository.updateDrink(drinkId, drinkData)
+        .then(result => {
+            res.redirect('/drinks');
+        });
+};
+
+exports.deleteDrink = (req, res, next) => {
+    const drinkId = req.params.drinkId;
+    DrinkRepository.deleteDrink(drinkId)
+        .then(() => {
+            res.redirect('/drinks');
+        });
+};
+
+exports.showAddProportionForm = (req, res, next) => {
+    let allDrinks, allIngre;
+    DrinkRepository.getDrinks()
+        .then(drinks => {
+            allDrinks = drinks;
+            return IngreadientRepository.getIngreadients();
+        })
+        .then(ingres => {
+            allIngre = ingres;
+            res.render('pages/proportion-form', {
+                proportion: {},
+                formMode: 'createNew',
+                allDrinks: allDrinks,
+                allIngres: allIngre,
+                pageTitle: 'Nowa proporcja',
+                btnLabel: 'Dodaj proporcje',
+                formAction: '/proportion/add',
+                navLocation: 'proportion'
+            });
+        });
 }
--- tin-projekt-Branicki-s18625/routes/drinkRoute.js
+++ tin-projekt-Branicki-s18625/routes/drinkRoute.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const drinkControler = require('../controllers/drinkController');
 router.get('/', drinkControler.showDrinkList);
 router.get('/add', drinkControler.showAddDrinkForm);
-router.get('/details/:drinkId?', drinkControler.showDrinkDetails);
+router.get('/details/:drinkId', drinkControler.showDrinkDetails);
+router.get('/edit/:drinkId', drinkControler.showEditDrinkForm);
+//
+router.post('/add', drinkControler.addDrink);
+router.post('/edit', drinkControler.updateDrink);
+router.get('/delete/:drinkId', drinkControler.deleteDrink);
 module.exports = router;
